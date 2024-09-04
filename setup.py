55a9e2368b788b3aebc319aded90891bc45bdfa9
oslearningbear <46176773+oslearningbear@users.noreply.github.com>
2024-09-04 16:18:56 -0700

    1.0.9

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="telemetry-sh",
-    version="1.0.8",
+    version="1.0.9",
     description="A simple telemetry logging SDK for Python.",
     author="JR R",
     author_email="thebuilderjr93@gmail.com",
